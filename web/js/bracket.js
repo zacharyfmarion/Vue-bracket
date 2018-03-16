f67a4f273adc65d8402762d3cc244200616eb835
Zachary Marion <zfm2@duke.edu>
2018-03-16 12:40:28 -0400

    Added data and processing

---
+++ web/js/bracket.js
@@ -0,0 +1,763 @@
+/**
+ * The JSON data structure that defines a team
+ */
+const tournament = [
+  [
+    [
+      {
+        id: '1.1',
+        name: 'St Johns',
+        winner: true,
+      },
+      {
+        id: '1.1',
+        name: 'Southern',
+      },
+    ],
+    [
+      {
+        id: '1.2',
+        name: 'VCU',
+        winner: true,
+      },
+      {
+        id: '1.2',
+        name: 'Marshall',
+      },
+    ],
+    [
+      {
+        id: '1.3',
+        name: 'NC State',
+        winner: true,
+      },
+      {
+        id: '1.3',
+        name: 'Nevada',
+      },
+    ],
+    [
+      {
+        id: '1.4',
+        name: 'UNLV',
+        winner: true,
+      },
+      {
+        id: '1.4',
+        name: 'San Diego St',
+      },
+    ],
+    [
+      {
+        id: '1.5',
+        name: 'Washington',
+        winner: true,
+      },
+      {
+        id: '1.5',
+        name: 'Kentucky',
+      },
+    ],
+    [
+      {
+        id: '1.6',
+        name: 'Tulsa',
+        winner: true,
+      },
+      {
+        id: '1.6',
+        name: 'UTEP',
+      },
+    ],
+    [
+      {
+        id: '1.7',
+        name: 'Alabama',
+        winner: true,
+      },
+      {
+        id: '1.7',
+        name: 'Arizona',
+      },
+    ],
+    [
+      {
+        id: '1.8',
+        name: 'Iowa',
+        winner: true,
+      },
+      {
+        id: '1.8',
+        name: 'Arkansas',
+      },
+    ],
+    [
+      {
+        id: '2.1',
+        name: 'Georgetown',
+        winner: true,
+      },
+      {
+        id: '2.1',
+        name: 'Lehigh',
+      },
+    ],
+    [
+      {
+        id: '2.2',
+        name: 'Georgia Tech',
+        winner: true,
+      },
+      {
+        id: '2.2',
+        name: 'Mercer',
+      },
+    ],
+    [
+      {
+        id: '2.3',
+        name: 'Illinois',
+        winner: true,
+      },
+      {
+        id: '2.3',
+        name: 'Northeastern',
+      },
+    ],
+    [
+      {
+        id: '2.4',
+        name: 'Loyola Illinois',
+        winner: true,
+      },
+      {
+        id: '2.4',
+        name: 'Iona',
+      },
+    ],
+    [
+      {
+        id: '2.5',
+        name: 'SMU',
+        winner: true,
+      },
+      {
+        id: '2.5',
+        name: 'Old Dominion',
+      },
+    ],
+    [
+      {
+        id: '2.6',
+        name: 'Georgia',
+        winner: true,
+      },
+      {
+        id: '2.6',
+        name: 'Wichita St',
+      },
+    ],
+    [
+      {
+        id: '2.7',
+        name: 'Syracuse',
+        winner: true,
+      },
+      {
+        id: '2.7',
+        name: 'DePaul',
+      },
+    ],
+    [
+      {
+        id: '2.8',
+        name: 'Temple',
+        winner: true,
+      },
+      {
+        id: '2.8',
+        name: 'Virginia Tech',
+      },
+    ],
+    [
+      {
+        id: '3.1',
+        name: 'Oklahoma',
+        winner: true,
+      },
+      {
+        id: '3.1',
+        name: 'North Carolina A&T',
+      },
+    ],
+    [
+      {
+        id: '3.2',
+        name: 'Memphis',
+        winner: true,
+      },
+      {
+        id: '3.2',
+        name: 'Pennsylvania',
+      },
+    ],
+    [
+      {
+        id: '3.3',
+        name: 'Duke',
+        winner: true,
+      },
+      {
+        id: '3.3',
+        name: 'Pepperdine',
+      },
+    ],
+    [
+      {
+        id: '3.4',
+        name: 'Ohio St',
+        winner: true,
+      },
+      {
+        id: '3.4',
+        name: 'Iowa St',
+      },
+    ],
+    [
+      {
+        id: '3.5',
+        name: 'Louisiana Tech',
+        winner: true,
+      },
+      {
+        id: '3.5',
+        name: 'Pittsburgh',
+      },
+    ],
+    [
+      {
+        id: '3.6',
+        name: 'Texas Tech',
+        winner: true,
+      },
+      {
+        id: '3.6',
+        name: 'Boston College',
+      },
+    ],
+    [
+      {
+        id: '3.7',
+        name: 'UAB',
+        winner: true,
+      },
+      {
+        id: '3.7',
+        name: 'Michigan St',
+      },
+    ],
+    [
+      {
+        id: '3.8',
+        name: 'USC',
+        winner: true,
+      },
+      {
+        id: '3.8',
+        name: 'Illinois St',
+      },
+    ],
+    [
+      {
+        id: '4.1',
+        name: 'Michigan',
+        winner: true,
+      },
+      {
+        id: '4.1',
+        name: 'Fairleigh Dickinson',
+      },
+    ],
+    [
+      {
+        id: '4.2',
+        name: 'North Carolina',
+        winner: true,
+      },
+      {
+        id: '4.2',
+        name: 'Middle Tennessee St',
+      },
+    ],
+    [
+      {
+        id: '4.3',
+        name: 'Kansas',
+        winner: true,
+      },
+      {
+        id: '4.3',
+        name: 'Ohio',
+      },
+    ],
+    [
+      {
+        id: '4.4',
+        name: 'LSU',
+        winner: true,
+      },
+      {
+        id: '4.4',
+        name: 'Navy',
+      },
+    ],
+    [
+      {
+        id: '4.5',
+        name: 'Maryland',
+        winner: true,
+      },
+      {
+        id: '4.5',
+        name: 'Miami Ohio',
+      },
+    ],
+    [
+      {
+        id: '4.6',
+        name: 'Purdue',
+        winner: true,
+      },
+      {
+        id: '4.6',
+        name: 'Auburn',
+      },
+    ],
+    [
+      {
+        id: '4.7',
+        name: 'Notre Dame',
+        winner: true,
+      },
+      {
+        id: '4.7',
+        name: 'Oregon St',
+      },
+    ],
+    [
+      {
+        id: '4.8',
+        name: 'Villanova',
+        winner: true,
+      },
+      {
+        id: '4.8',
+        name: 'Dayton',
+      },
+    ],
+  ],
+  [
+    [
+      {
+        id: '1.1',
+        name: 'St Johns',
+        winner: true,
+      },
+      {
+        id: '1.1',
+        name: 'Arkansas',
+      },
+    ],
+    [
+      {
+        id: '1.2',
+        name: 'VCU',
+        winner: true,
+      },
+      {
+        id: '1.2',
+        name: 'Alabama',
+      },
+    ],
+    [
+      {
+        id: '1.3',
+        name: 'NC State',
+        winner: true,
+      },
+      {
+        id: '1.3',
+        name: 'UTEP',
+      },
+    ],
+    [
+      {
+        id: '1.4',
+        name: 'UNLV',
+        winner: true,
+      },
+      {
+        id: '1.4',
+        name: 'Kentucky',
+      },
+    ],
+    [
+      {
+        id: '2.1',
+        name: 'Georgetown',
+        winner: true,
+      },
+      {
+        id: '2.1',
+        name: 'Temple',
+      },
+    ],
+    [
+      {
+        id: '2.2',
+        name: 'Georgia Tech',
+        winner: true,
+      },
+      {
+        id: '2.2',
+        name: 'Syracuse',
+      },
+    ],
+    [
+      {
+        id: '2.3',
+        name: 'Illinois',
+        winner: true,
+      },
+      {
+        id: '2.3',
+        name: 'Georgia',
+      },
+    ],
+    [
+      {
+        id: '2.4',
+        name: 'Loyola Illinois',
+        winner: true,
+      },
+      {
+        id: '2.4',
+        name: 'SMU',
+      },
+    ],
+    [
+      {
+        id: '3.1',
+        name: 'Oklahoma',
+        winner: true,
+      },
+      {
+        id: '3.1',
+        name: 'Illinois St',
+      },
+    ],
+    [
+      {
+        id: '3.2',
+        name: 'Memphis',
+        winner: true,
+      },
+      {
+        id: '3.2',
+        name: 'UAB',
+      },
+    ],
+    [
+      {
+        id: '3.3',
+        name: 'Duke',
+        winner: true,
+      },
+      {
+        id: '3.3',
+        name: 'Boston College',
+      },
+    ],
+    [
+      {
+        id: '3.4',
+        name: 'Ohio St',
+        winner: true,
+      },
+      {
+        id: '3.4',
+        name: 'Louisiana Tech',
+      },
+    ],
+    [
+      {
+        id: '4.1',
+        name: 'Michigan',
+        winner: true,
+      },
+      {
+        id: '4.1',
+        name: 'Villanova',
+      },
+    ],
+    [
+      {
+        id: '4.2',
+        name: 'North Carolina',
+        winner: true,
+      },
+      {
+        id: '4.2',
+        name: 'Notre Dame',
+      },
+    ],
+    [
+      {
+        id: '4.3',
+        name: 'Kansas',
+        winner: true,
+      },
+      {
+        id: '4.3',
+        name: 'Auburn',
+      },
+    ],
+    [
+      {
+        id: '4.13',
+        name: 'Navy',
+        winner: true,
+      },
+      {
+        id: '4.13',
+        name: 'Maryland',
+      },
+    ],
+  ],
+  [
+    [
+      {
+        id: '1.1',
+        name: 'St Johns',
+        winner: true,
+      },
+      {
+        id: '1.1',
+        name: 'Kentucky',
+      },
+    ],
+    [
+      {
+        id: '1.7',
+        name: 'Alabama',
+        winner: true,
+      },
+      {
+        id: '1.7',
+        name: 'NC State',
+      },
+    ],
+    [
+      {
+        id: '2.1',
+        name: 'Georgetown',
+        winner: true,
+      },
+      {
+        id: '2.1',
+        name: 'Loyola Illinois',
+      },
+    ],
+    [
+      {
+        id: '2.2',
+        name: 'Georgia Tech',
+        winner: true,
+      },
+      {
+        id: '2.2',
+        name: 'Illinois',
+      },
+    ],
+    [
+      {
+        id: '3.1',
+        name: 'Oklahoma',
+        winner: true,
+      },
+      {
+        id: '3.1',
+        name: 'Louisiana Tech',
+      },
+    ],
+    [
+      {
+        id: '3.2',
+        name: 'Memphis',
+        winner: true,
+      },
+      {
+        id: '3.2',
+        name: 'Boston College',
+      },
+    ],
+    [
+      {
+        id: '4.8',
+        name: 'Villanova',
+        winner: true,
+      },
+      {
+        id: '4.8',
+        name: 'Maryland',
+      },
+    ],
+    [
+      {
+        id: '4.2',
+        name: 'North Carolina',
+        winner: true,
+      },
+      {
+        id: '4.2',
+        name: 'Auburn',
+      },
+    ],
+  ],
+  [
+    [
+      {
+        id: '1.1',
+        name: 'St Johns',
+        winner: true,
+      },
+      {
+        id: '1.1',
+        name: 'NC State',
+      },
+    ],
+    [
+      {
+        id: '2.1',
+        name: 'Georgetown',
+        winner: true,
+      },
+      {
+        id: '2.1',
+        name: 'Georgia Tech',
+      },
+    ],
+    [
+      {
+        id: '3.1',
+        name: 'Oklahoma',
+        winner: true,
+      },
+      {
+        id: '3.1',
+        name: 'Memphis',
+      },
+    ],
+    [
+      {
+        id: '4.8',
+        name: 'Villanova',
+        winner: true,
+      },
+      {
+        id: '4.8',
+        name: 'North Carolina',
+      },
+    ],
+  ],
+  [
+    [
+      {
+        id: '1.1',
+        name: 'St Johns',
+        winner: true,
+      },
+      {
+        id: '1.1',
+        name: 'Georgetown',
+      },
+    ],
+    [
+      {
+        id: '2.2',
+        name: 'Memphis',
+        winner: true,
+      },
+      {
+        id: '2.2',
+        name: 'Villanova',
+      },
+    ],
+  ],
+  [
+    [
+      {
+        id: '1.1',
+        name: 'Georgetown',
+        winner: true,
+      },
+      {
+        id: '1.1',
+        name: 'Villanova',
+      },
+    ],
+  ],
+];
+
+Vue.component('bracket-team', {
+  props: {
+    team: {
+      type: Object,
+    },
+    bottom: {
+      type: Boolean,
+    },
+  },
+  template: `
+    <li class="game" :class="{'game-top': !bottom, 'game-bottom': bottom, winner: team && team.winner}">
+      <span v-if="team">{{team.name}}</span> 
+      <button v-else>Testing</button>
+    </li>
+  `,
+});
+
+Vue.component('vue-bracket', {
+  props: {
+    /**
+     * An array of rounds, which are themselves arrays
+     * of individual matches
+     */
+    tournament: {
+      type: Array,
+      required: true,
+    },
+  },
+  template: `
+    <div class="tournament">
+      <ul class="round" v-for="round in tournament">
+        <template v-for="(match, index) in round" v-if="match.length > 1">
+          <li class="spacer">&nbsp;</li>
+          <bracket-team :team="match[0]"></bracket-team>
+          <li class="game game-spacer">&nbsp;</li>
+          <bracket-team :team="match[1]" bottom></bracket-team>
+          <li class="spacer" v-if="index === round.length -1">&nbsp;</li>
+        </template>
+        <template v-for="(match, index) in round" v-if="match.length === 1">
+          <bracket-team :team="match[0]"></bracket-team>
+        </template>
+      </ul>
+    </div> 
+  `,
+});
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    tournament: tournament,
+  },
+});
